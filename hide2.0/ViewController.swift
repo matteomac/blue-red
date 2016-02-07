d7cf112e6ee4e8e51510bc3c32099da1bb40f3af
Matteo Barni <matteobarni92@kirk.local>
2016-02-07 12:21:47 +0100

    Initial Commit

---
+++ hide2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hide2.0
+//
+//  Created by matteo barni on 07/02/16.
+//  Copyright © 2016 proetiam srl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
