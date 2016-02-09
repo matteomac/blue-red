2405ba98b077474176a5aa9be969a360bc46fd40
matteo barni <matteobarni92@Kirk.local>
2016-02-09 11:13:38 +0100

    first add hide2.0

--- hide2.0/ViewController.swift
+++ hide2.0/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fiamma: UIImageView!
+    @IBOutlet weak var goccia: UIImageView!
+    
+    @IBOutlet weak var HideR: UIButton!
+    @IBOutlet weak var HideB: UIButton!
+    
+    @IBOutlet weak var ShowR: UIButton!
+    @IBOutlet weak var ShowB: UIButton!
+    
+    
+    //hide actions
+    @IBAction func ActionHR(sender: AnyObject) {
+        fiamma.hidden=true
+        HideR.hidden=true
+        ShowR.hidden=false
+    }
+    @IBAction func ActionHB(sender: AnyObject) {
+        goccia.hidden=true
+        HideB.hidden=true
+        ShowB.hidden=false
+    }
+    //show actions
+    @IBAction func ActionSR(sender: AnyObject) {
+        fiamma.hidden=false
+        HideR.hidden=false
+        ShowR.hidden=true
+    }
+    @IBAction func ActionSB(sender: AnyObject) {
+        goccia.hidden=false
+        HideB.hidden=false
+        ShowB.hidden=true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
